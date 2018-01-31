5adf72fbfca696b9288c4e657d95fb45a973fed3
Vittorio Adamo <vittorio.adamo@arreeba.com>
2018-01-31 21:09:01 +0100

    Add search types

---
+++ src/client/types/redux/search.js
@@ -0,0 +1,3 @@
+export const ARTICLES_SEARCH_START = 'articles/SEARCH_START';
+export const ARTICLES_SEARCH_SUCCESS = 'articles/SEARCH_SUCCESS';
+export const ARTICLES_SEARCH_ERROR = 'articles/SEARCH_ERROR';
